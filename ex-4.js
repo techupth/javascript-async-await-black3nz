08b455fa5eeddee7281ec124558dac2deae71268
black3nz <pawich.p@hotmail.com>
2024-05-02 11:49:51 +0700

    Submit Assignment

--- ex-4.js
+++ ex-4.js
@@ -13,3 +13,13 @@ let getJohnProfile = () => {
   });
 };
 // Start coding here
+async function displayProfile() {
+  try {
+    const profile = await getJohnProfile();
+    console.log(profile);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+displayProfile();
